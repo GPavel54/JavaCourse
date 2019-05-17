06562811785fb380b30564951f364dafd642eda3
GPavel54 <gpavel5497@gmail.com>
2019-05-17 23:43:57 +0700

    From server upper case string

---
+++ toUpper/client/src/main/java/Client.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+
+    System.out.println("Welcome to Client side");
+
+    Socket fromserver;
+
+    System.out.println("Connecting to ... 127.0.0.1");
+
+    fromserver = new Socket("127.0.0.1", 8080);
+    BufferedReader in = new BufferedReader(new InputStreamReader(fromserver.getInputStream()));
+    PrintWriter out = new PrintWriter(fromserver.getOutputStream(), true);
+    BufferedReader inu = new BufferedReader(new InputStreamReader(System.in));
+
+    String fuser, fserver;
+
+    while ((fuser = inu.readLine()) != null) {
+      out.println(fuser);
+      fserver = in.readLine();
+      System.out.println(fserver);
+      if (fuser.equalsIgnoreCase("close"))
+        break;
+      if (fuser.equalsIgnoreCase("exit"))
+        break;
+    }
+
+    out.close();
+    in.close();
+    inu.close();
+    fromserver.close();
+  }
+}
